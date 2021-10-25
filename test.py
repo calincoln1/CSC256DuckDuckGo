000ae8e1eca94d0655c5d2c0437d03145c8d7ebd
calincoln1 <calincoln1@my.waketech.edu>
2021-10-24 20:37:49 -0400

    Changed test.py

--- test.py
+++ test.py
@@ -1,8 +1,99 @@
+# Imports
 import requests
 
-url_ddg = "https://api.duckduckgo.com"
+# !!!REQ!!! Program correctly searches the DuckDuckGo api for the search string (see test_ddg0)
+# DuckDuckGo (DDG) API info
+api_url  = "https://api.duckduckgo.com"
+api_path = "/?q=presidents of the united states&format=json"
 
+# List of Presidents
+presidents = [
+    "Washington",   "Adams",        "Jefferson",    "Madison",
+    "Monroe",       "Jackson",      "Buren",        "Tyler",
+    "Polk",         "Taylor",       "Fillmore",     "Pierce",
+    "Buchanan",     "Lincoln",      "Grant",        "Hayes",
+    "Garfield",     "Arthur",       "Cleveland",    "Harrison",
+    "McKinley",     "Roosevelt",    "Taft",         "Wilson",
+    "Harding",      "Coolidge",     "Hoover",       "Truman",
+    "Eisenhower",   "Kennedy",      "Johnson",      "Nixon",
+    "Ford",         "Carter",       "Reagan",       "Bush",
+    "Clinton",      "Obama",        "Trump",        "Biden"
+]
+
+# Test
 def test_ddg0():
-    resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
-    rsp_data = resp.json()
-    assert "DuckDuckGo" in rsp_data["Heading"]
+
+    # Print first to make the output prettier
+    print()
+
+    # Query DDG API
+    resp = requests.get(api_url + api_path) # !!!REQ!!! Program searches the DuckDuckGo api only once
+
+    # Get RelatedTopics from
+    resp_related_topics = resp.json()["RelatedTopics"]
+
+    # Make Text string from RelatedTopics
+    text = ""
+    for i in resp_related_topics:
+        text += i["Text"] + '\n'
+
+    # Check to make sure all presidents
+    #  are in the response
+    error_count = 0
+    for pres in presidents:  # !!!REQ!!! program tests that each president is present in the response
+        if pres not in text:
+            error_count += 1
+        else:
+            print(pres + " found!")
+
+=======
+# Imports
+import requests
+
+# !!!REQ!!! Program correctly searches the DuckDuckGo api for the search string (see test_ddg0)
+# DuckDuckGo (DDG) API info
+api_url  = "https://api.duckduckgo.com"
+api_path = "/?q=presidents of the united states&format=json"
+
+# List of Presidents
+presidents = [
+    "Washington",   "Adams",        "Jefferson",    "Madison",
+    "Monroe",       "Jackson",      "Buren",        "Harrison",
+    "Tyler",        "Polk",         "Taylor",       "Fillmore",
+    "Pierce",       "Buchanan",     "Lincoln",      "Johnson",
+    "Grant",        "Hayes",        "Garfield",     "Arthur",
+    "Cleveland",    "Harrison",     "Cleveland",    "McKinley",
+    "Roosevelt",    "Taft",         "Wilson",       "Harding",
+    "Coolidge",     "Hoover",       "Roosevelt",    "Truman",
+    "Eisenhower",   "Kennedy",      "Johnson",      "Nixon",
+    "Ford",         "Carter",       "Reagan",       "Bush",
+    "Clinton",      "Obama",        "Trump",        "Biden"
+]
+
+# Test
+def test_ddg0():
+
+    # Print first to make the output prettier
+    print()
+
+    # Query DDG API
+    resp = requests.get(api_url + api_path) # !!!REQ!!! Program searches the DuckDuckGo api only once
+
+    # Get RelatedTopics from
+    resp_related_topics = resp.json()["RelatedTopics"]
+
+    # Make Text string from RelatedTopics
+    text = ""
+    for i in resp_related_topics:
+        text += i["Text"] + '\n'
+
+    # Check to make sure all presidents
+    #  are in the response
+    error_count = 0
+    for pres in presidents:  # !!!REQ!!! program tests that each president is present in the response
+        if pres not in text:
+            error_count += 1
+        else:
+            print(pres + " found!")
+
+    assert error_count == 0
