fc7c71c330f0798e3e7cf09c6dbc492618d5d211
calincoln1 <calincoln1@my.waketech.edu>
2021-10-24 20:35:21 -0400

    Changed test.py

--- test.py
+++ test.py
@@ -9,13 +9,12 @@ api_path = "/?q=presidents of the united states&format=json"
 # List of Presidents
 presidents = [
     "Washington",   "Adams",        "Jefferson",    "Madison",
-    "Monroe",       "Jackson",      "Buren",        "Harrison",
-    "Tyler",        "Polk",         "Taylor",       "Fillmore",
-    "Pierce",       "Buchanan",     "Lincoln",      "Johnson",
-    "Grant",        "Hayes",        "Garfield",     "Arthur",
-    "Cleveland",    "Harrison",     "Cleveland",    "McKinley",
-    "Roosevelt",    "Taft",         "Wilson",       "Harding",
-    "Coolidge",     "Hoover",       "Roosevelt",    "Truman",
+    "Monroe",       "Jackson",      "Buren",        "Tyler",
+    "Polk",         "Taylor",       "Fillmore",     "Pierce",
+    "Buchanan",     "Lincoln",      "Grant",        "Hayes",
+    "Garfield",     "Arthur",       "Cleveland",    "Harrison",
+    "McKinley",     "Roosevelt",    "Taft",         "Wilson",
+    "Harding",      "Coolidge",     "Hoover",       "Truman",
     "Eisenhower",   "Kennedy",      "Johnson",      "Nixon",
     "Ford",         "Carter",       "Reagan",       "Bush",
     "Clinton",      "Obama",        "Trump",        "Biden"
